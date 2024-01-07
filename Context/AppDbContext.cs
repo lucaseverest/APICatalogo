14fb05dee83c3016638c19dd129140f091846c3d
lucaseverest <lucaseverest10@gmail.com>
2024-01-07 19:52:38 -0300

    Add project files.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public string DbPath { get; }
+        public AppDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "APICatalogo.db");
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+
+    }
+}
